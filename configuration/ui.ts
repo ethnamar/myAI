6c04733f9f73772973f3cbfd070550ac9b78383e
ethnamar <122567494+ethnamar@users.noreply.github.com>
2025-03-04 16:22:23 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CHAT_HEADER: string = `FUNCTIONAL FITNESS AI ASSISTANT 2025`;
+export const MESSAGE_PLACEHOLDER: string = `Ask me about functional fitness workout advice`;
+export const FOOTER_MESSAGE: string = `This AI can make mistakes; please double check it's responses`;
+export const CLEAR_BUTTON_TEXT: string = `Powered by AI - Developed by Ethan Elaasar`;
+export const PAGE_TITLE: string = `Functional Fitness AI Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
