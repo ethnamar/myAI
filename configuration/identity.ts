c2ed89e6b41964fd6aec21f6e1d696638d31d4bf
ethnamar <122567494+ethnamar@users.noreply.github.com>
2025-03-04 16:28:45 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Ethan Elaasar`;
+export const OWNER_DESCRIPTION: string = `Senior Business Student`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Ford`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
