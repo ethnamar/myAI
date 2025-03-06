23a3fe03f13b8205250b2dcdc8246c9be107ea8b
ethnamar <122567494+ethnamar@users.noreply.github.com>
2025-03-06 14:09:58 -0500

    Create metadata.ts
    new file

---
+++ app/metadata.ts
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+import { PAGE_TITLE, PAGE_DESCRIPTION } from "@/configuration/ui";
+
+export const metadata: Metadata = {
+  title: PAGE_TITLE,
+  description: PAGE_DESCRIPTION,
+};
