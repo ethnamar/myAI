2bcf11ca6896ba57293b86694d897f779e4201de
ethnamar <122567494+ethnamar@users.noreply.github.com>
2025-03-06 14:13:06 -0500

    Delete app/metadata.ts

--- app/metadata.ts
+++
@@ -1,7 +0,0 @@
-import type { Metadata } from "next";
-import { PAGE_TITLE, PAGE_DESCRIPTION } from "@/configuration/ui";
-
-export const metadata: Metadata = {
-  title: PAGE_TITLE,
-  description: PAGE_DESCRIPTION,
-};
